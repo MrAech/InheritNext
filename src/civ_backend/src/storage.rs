f9f623d8b8c8229536046251d5f0b193e61d4e7e
Gauri Singh <149578403+gaurisingh73@users.noreply.github.com>
2025-08-24 22:49:08 +0530

    Revert "30 modularize retry system with adaptive per kind backoff"

--- src/civ_backend/src/storage.rs
+++
@@ -1,8 +0,0 @@
-use crate::models::User;
-use std::cell::RefCell;
-use std::collections::HashMap;
-
-
-thread_local! {
-    pub static USERS: RefCell<HashMap<String, User>> = RefCell::new(HashMap::new());
-}
